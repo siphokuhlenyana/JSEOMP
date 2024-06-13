47a20869e925f8668c2a04cfb91b01ea86315f5f
Siphokuhle <siphokuhlenyana@gmail.com>
2024-06-13 10:22:16 +0200

    Home page

---
+++ script/code.js
@@ -0,0 +1,8 @@
+let contents=document.querySelector('#main')
+contents.innerHTML += `
+
+<div class="slogan" id="slogan"> BUY . SELL . DO IT ALL OVER!! <br></div>
+
+<p id="introp">Welcome to the community of thrifted , Dagama prints and vintage wear .<br> Wallet's Best Friend!<br> Heaven's door  is an award-winning collection of thrifted clothing and one of many african prints .<br> The perfect blend of art and culture represents unity and connection .</p>
+      
+`
